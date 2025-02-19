e841e683984703a7e28f618595268e109e386392
frankie34-ctrl <alutafrankie830@gmail.com>
2025-02-19 13:31:41 +0300

    initial commit

--- Shopapp/urls.py
+++ Shopapp/urls.py
@@ -5,7 +5,7 @@ from Shopapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Home/', views.index),
-    path('about/', views.about),
-    path('gallery/', views.image),
+    path('home/', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('gallery/', views.image, name='gallery'),
 ]
